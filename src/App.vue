a2e36ccd52256bfc427b502ed156e8b70eeb6b7c
Andrew Hale <andyhalebmx@hotmail.com>
2019-10-02 16:47:26 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Products v-bind:products="allProducts" />
+    <!-- <Parent /> -->
+  </div>
+</template>
+
+<script>
+import json from './utils/sample.json';
+import Products from './components/Products.vue';
+import Parent from './components/Parent.vue';
+
+
+
+const getDiscount = json[0].discountPercent;
+const getProducts = json[0].products;
+
+export default{
+  name: "App",
+  components: {
+    Products,
+    Parent
+  },
+    data(){
+        return{
+            discount: getDiscount,
+            allProducts: getProducts
+        }
+    }
+}
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  /* background-color: mistyrose; */
+  padding: 2rem;
+}
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-bind:key="product.id" v-for="product in products">
+      <h3>{{ product.name }}</h3>
+      {{ product.description }}
+      <p>{{ product.price }}</p>
+      <button>ADD TO CART</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Products",
+    props: ["products"]
+}
+</script>
+
+<style>
+
+</style>
